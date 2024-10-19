fa49e16240a512397692308830cfc4e3986b5eca
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2024-10-19 23:24:00 +0530

    added bit manipulation question

--- 8. Bit Manipulation/8.1 Learn Bit Manipulation/8.1.8 Divide two integers without using multiplication, division and mod operator.py
+++ 8. Bit Manipulation/8.1 Learn Bit Manipulation/8.1.8 Divide two integers without using multiplication, division and mod operator.py
@@ -1,3 +1,27 @@
+"""
+Q. Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+Return the quotient after dividing dividend by divisor.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+ 
+Example 1:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = 3.33333.. which is truncated to 3.
+
+Example 2:
+
+Input: dividend = 7, divisor = -3
+Output: -2
+Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+"""
+
+
 def divide(dividend, divisor):
     """
     1. Brute Force Approach
@@ -6,6 +30,23 @@ def divide(dividend, divisor):
     4. Space Complexity: O(1)
     """
 
+    INT_MAX = 2**31 - 1
+    INT_MIN = -2**31
+
+    if dividend == divisor:
+        return 1
+
+    sign = True
+    if dividend < 0 and divisor < 0:
+        dividend = -dividend
+        divisor = -divisor
+    elif dividend < 0:
+        dividend = -dividend
+        sign = False
+    elif divisor < 0:
+        divisor = -divisor
+        sign = False
+
     counter = 0
     summ = 0
 
@@ -13,7 +54,18 @@ def divide(dividend, divisor):
         summ += divisor
         counter += 1
 
-    return counter - 1
+    counter = counter - 1
+
+    if not sign:
+        counter = -counter
+    
+    if counter < INT_MIN:
+        return INT_MIN
+    
+    if counter > INT_MAX:
+        return INT_MAX
+
+    return counter
 
 
 def divideOptimal(dividend, divisor):
