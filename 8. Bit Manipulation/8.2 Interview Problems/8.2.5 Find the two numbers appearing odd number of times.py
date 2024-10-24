2ae1445edbb884adba2a12a939553adbca13365e
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2024-10-24 22:36:08 +0530

    added question

--- 8. Bit Manipulation/8.2 Interview Problems/8.2.5 Find the two numbers appearing odd number of times.py
+++ 8. Bit Manipulation/8.2 Interview Problems/8.2.5 Find the two numbers appearing odd number of times.py
@@ -1,3 +1,23 @@
+"""
+Q. Given an unsorted array, Arr[] of size N and that contains even number of occurrences for all numbers except two numbers. Find the two numbers in decreasing order which has odd occurrences.
+
+Example 1:
+
+Input:
+N = 8
+Arr = {4, 2, 4, 5, 2, 3, 3, 1}
+Output: {5, 1} 
+Explanation: 5 and 1 have odd occurrences.
+
+Example 2:
+
+Input:
+N = 8
+Arr = {1 7 5 7 5 4 7 4}
+Output: {7, 1}
+Explanation: 7 and 1 have odd occurrences.
+"""
+
 def twoOddNum(Arr, N):
     """
     1. Optimal Approach
@@ -23,8 +43,6 @@ def twoOddNum(Arr, N):
             bucket2 = bucket2 ^ num
     
     return bucket1, bucket2
-
-
 
 Arr = [4, 2, 4, 5, 2, 3, 3, 1]
 N = 8
