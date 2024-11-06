997019756ec4785b824d97d43e625bdf1eda7735
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2024-11-06 22:16:53 +0530

    added Questions

---
+++ 9. Stack and Queues/9.3 Monotonic Stack Queue Problems/9.3.6 Asteroid Collision.py
@@ -0,0 +1,52 @@
+"""
+Q. We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+"""
+
+def asteroidCollision(asteroids):
+    """
+    1. iterate till n
+    2. if positive element found push in stack
+    3. if negative element found keep poping (if -ve element is greater)
+    4. if equal element found then also pop
+    5. edge case: if there is no element or top element of stack is negative push in stack
+    6. Time Complexity: O(N+N)
+    6. Space Complexity: O(N)
+    """
+    n = len(asteroids)
+
+    stack = []
+
+    for i in range(n):
+        if asteroids[i] > 0:
+            stack.append(asteroids[i])
+        else:
+            while stack and stack[-1] > 0 and abs(asteroids[i]) > stack[-1]:
+                stack.pop()
+            if stack and abs(asteroids[i]) == stack[-1]:
+                stack.pop()
+            elif not stack or stack[-1] < 0:
+                stack.append(asteroids[i])
+    return stack
+
+asteroids = [-2,-2,1,-2]
+print(asteroidCollision(asteroids))
--- Test.py
+++ Test.py
@@ -1,154 +1,18 @@
-def sumSubarrayMinsBrute(arr):
-    n = len(arr)
-    summ = 0
-
-    for i in range(n):
-        for j in range(i, n):
-            summ += min(arr[i:j+1])
-    return summ
-
-def sumSubarrayMinsBetter(arr):
-    n = len(arr)
-    summ = 0
-
-    for i in range(n):
-        minn = arr[i]
-        for j in range(i, n):
-            minn = min(minn, arr[j])
-            summ += minn
-    return summ
-
-
-def nextSmallerElement(arr):
-    n = len(arr)
+def asteroidCollision(asteroids):
+    n = len(asteroids)
     stack = []
-    ans = [n]*n # Initialize with `n` to represent the boundary (no smaller element found)
-    for i in range(n-1, -1, -1):
-        while stack and arr[i] <= arr[stack[-1]]:
-            stack.pop()
-        ans[i] = stack[-1] if stack else n
-        stack.append(i)
-    return ans
-
-
-def prevSmallerEqualElement(arr):
-    n = len(arr)
-    stack = []
-    ans = [-1]*n # Initialize with `-1` to represent no previous smaller element
 
     for i in range(n):
-        while stack and arr[i] < arr[stack[-1]]:
-            stack.pop()
-        ans[i] = stack[-1] if stack else -1
-        stack.append(i)
-    return ans
+        if asteroids[i] > 0:
+            stack.append(asteroids[i])
+        else:
+            while stack and stack[-1] > 0 and abs(asteroids[i]) > stack[-1]:
+                stack.pop()
+            if stack and abs(asteroids[i]) == stack[-1]:
+                stack.pop()
+            elif not stack or stack[-1] < 0:
+                stack.append(asteroids[i])
+    return stack
 
-
-def sumSubarrayMinsOptimal(arr):
-    """
-    Step1: find next smaller element
-    Step2: find prev smaller element
-    Step3: find distance from left and right
-    Step4: now multiply left*right*number and add to sum
-    Time Complexity: O(N+N+N)
-    Space Complexity: O(2N+2N)
-    """
-    n = len(arr)
-    prev = prevSmallerEqualElement(arr)
-    after = nextSmallerElement(arr)
-    ans = 0
-    mod = 10 ** 9 + 7
-    for i in range(n):
-        left = i - prev[i]
-        right = after[i] - i
-        ans = (ans + (((left * right)%mod) * arr[i]) % mod)%mod
-    return ans
-
-
-
-arr = [1,1]
-print(prevSmallerEqualElement(arr))
-print(nextSmallerElement(arr))
-print(sumSubarrayMinsOptimal(arr))
-
-"""
-Intution:
-arr = [3,1,2,4]
-
-subarrays
-
-3           = 3
-3,1         = 1
-3,1,2       = 1
-3,1,2,4     = 1
-
-1           = 1
-1,2         = 1
-1,2,4       = 1
-
-2           = 2
-2,4         = 2
-
-4           = 4
-
-contributions
-x1  x6 x2 x1
-[3, 1, 2, 4]
-
-now problem is brokendown intto find the contributions
-
-          0 1 2 3 4 5 6 7
-consider [1,4,6,7,3,7,8,1]
-
-contribution of 3 in ans on right side (next smaller element is 1 so ignoring 7th index)
-[3]
-[3,7]
-[3,7,8]
-
-contribution of 3 in ans on left side (prev smaller element is 1 so ignoring 0th index)
-[4,6,7,3]
-[6,7,3]
-[7,3]
-[3] #already counted
-
-mid contribution
-
-[3] #already counted
-[7,3,7]
-[6,7,3,7]
-[7,3,7,8]
-[4,6,7,3,7]
-[6,7,3,7,8]
-[4,6,7,3,7,8]
-
-for final is left * right = 3 * 4 = 12
-
-Find Next smaller element
-
-
-Step1: find next smaller element
-Step2: find prev smaller element
-Step3: find distance from left and right
-Step4: now multiply left*right*number and add to sum
-
-Dry Run:
-
-arr = [3,1,2,4]
-prev smaller elements = [-1,-1,1,2]
-next smaller elements = [1,4,4,4]
-
-ans = [1*1*3, 2*3*1, 1*2*2, 1*1*4] = 17
-
-Edge Case:
-       0  1
-arr = [1, 1]
-nse =  2  2
-pse =  -1 0(considering prev smaller is on 0th to avoid double counting) 
-
-[1] -> 0 index
-[1,1]
-[1] -> 1 index
-
-contributions = 3 (need to fix double counting here)
-find prev smaller equal element
-"""
+asteroids = [-2,-2,1,-2]
+print(asteroidCollision(asteroids))
