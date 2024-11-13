f21e6216400fd64be9f7d18b11d5ad3ce557b714
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2024-11-13 17:32:31 +0530

    added new module

--- 10. Sliding Window & Two Pointer Combined Problems/10.1.1 Longest Substring Without Repeating Characters.py
+++ 10. Sliding Window & Two Pointer Combined Problems/10.1.1 Longest Substring Without Repeating Characters.py
@@ -1,3 +1,23 @@
+"""
+Q. Given a String, find the length of longest substring without any repeating character.
+
+Example 1:
+
+Input: s = ”abcabcbb”
+
+Output: 3
+
+Explanation: The answer is abc with length of 3.
+
+Example 2:
+
+Input: s = ”bbbbb”
+
+Output: 1
+
+Explanation: The answer is b with length of 1 units.
+"""
+
 def lengthOfLongestSubstringBrute(s):
     """
     Brute Force Approach
