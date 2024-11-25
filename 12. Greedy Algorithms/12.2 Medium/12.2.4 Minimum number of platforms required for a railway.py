a45f08fec1558d5c66d346ad8f055d6824fcd904
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2024-11-25 22:43:19 +0530

    added questions

--- 12. Greedy Algorithms/12.2 Medium/12.2.4 Minimum number of platforms required for a railway.py
+++ 12. Greedy Algorithms/12.2 Medium/12.2.4 Minimum number of platforms required for a railway.py
@@ -1,3 +1,25 @@
+"""
+Q. Given an array, jobs[] where each job[i] has a jobid, deadline and profit associated with it. Each job takes 1 unit of time to complete and only one job can be scheduled at a time. We earn the profit associated with a job if and only if the job is completed by its deadline.
+
+Find the number of jobs done and the maximum profit.
+
+Note: jobs will be given in the form (jobid, deadline, profit) associated with that job. Deadline of the job is the time on or before which job needs to be completed to earn the profit.
+
+Examples :
+
+Input: jobs[] = [(1,4,20), (2,1,1), (3,1,40), (4,1,30)]
+Output: [2, 60]
+Explanation: job1 and job3 can be done with maximum profit of 60 (20+40).
+
+Input: jobs[] = [(1,2,100), (2,1,19), (3,2,27), (4,1,25), (5,1,15)]
+Output: [2, 127]
+Explanation: 2 jobs can be done with maximum profit of 127 (100+27).
+
+Input: jobs[] = [(1,3,50), (2,1,10), (3,2,20), (4,2,30)]
+Output: [2, 80]
+Explanation: Job 1 and Job 4 can be completed with a maximum profit of 80 (50 + 30).
+"""
+
 def minimumPlatform(arr, dep):
     """
     Brute Force Approach
