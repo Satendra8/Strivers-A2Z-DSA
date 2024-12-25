b01da4687504d0ab482a881677ee16435f092406
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2024-12-25 21:25:05 +0530

    added more questions

--- 15. Graph/15.2 Problems on BFS|DFS/15.2.6 Number of Distinct Islands [dfs multisource].py
+++ 15. Graph/15.2 Problems on BFS|DFS/15.2.6 Number of Distinct Islands [dfs multisource].py
@@ -1,3 +1,26 @@
+"""
+Q. Given a grid of size n*m (n is the number of rows and m is the number of columns in the grid) consisting of '0's (Water) and '1's(Land). Find the number of islands.
+
+Note: An island is either surrounded by water or the boundary of a grid and is formed by connecting adjacent lands horizontally or vertically or diagonally i.e., in all 8 directions.
+
+Examples:
+
+Input: grid = [[0,1],[1,0],[1,1],[1,0]]
+
+Output: 1
+Explanation:
+The grid is-
+
+All lands are connected.
+Input: grid = [[0,1,1,1,0,0,0],[0,0,1,1,0,1,0]]
+
+Output: 2
+Expanation:
+The grid is-
+ 
+There are two islands :- one is colored in "blue" and other in "red".
+"""
+
 def bfsStriver(row, col, visited, m, n, grid):
     """
     for 8 direction row_range [-1,1] and col_range [-1,1]
