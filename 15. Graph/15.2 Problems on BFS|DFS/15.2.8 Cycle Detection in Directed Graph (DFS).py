20dad5c8d5868b5511039d26fb09846d34e96153
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2024-12-30 21:47:05 +0530

    added questions

--- 15. Graph/15.2 Problems on BFS|DFS/15.2.8 Cycle Detection in Directed Graph (DFS).py
+++ 15. Graph/15.2 Problems on BFS|DFS/15.2.8 Cycle Detection in Directed Graph (DFS).py
@@ -1,3 +1,21 @@
+"""
+Q. Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges, check whether it contains any cycle or not.
+The graph is represented as an adjacency list, where adj[i] contains a list of vertices that are directly reachable from vertex i. Specifically, adj[i][j] represents an edge from vertex i to vertex j.
+
+Example 1:
+
+Input:
+
+Output: 1
+Explanation: 3 -> 3 is a cycle
+
+Example 2:
+Input:
+
+Output: 0
+Explanation: no cycle in the graph
+"""
+
 def dfs(node, adj, visited, visitedPath, V):
     visited[node] = 1
     visitedPath[node] = 1
