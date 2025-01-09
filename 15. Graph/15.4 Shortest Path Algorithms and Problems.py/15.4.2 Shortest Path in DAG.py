3003de9bb4f7927872dd5a8877fe9fafcdd77e97
Satendra Kumar <satendrakumar2345.sk.sk@gmail.com>
2025-01-09 08:01:25 +0530

    added questions

--- 15. Graph/15.4 Shortest Path Algorithms and Problems.py/15.4.2 Shortest Path in DAG.py
+++ 15. Graph/15.4 Shortest Path Algorithms and Problems.py/15.4.2 Shortest Path in DAG.py
@@ -1,3 +1,18 @@
+"""
+Q. Given a Directed Acyclic Graph of V vertices from 0 to n-1 and a 2D Integer array(or vector) edges[ ][ ] of length E, where there is a directed edge from edge[i][0] to edge[i][1] with a distance of edge[i][2] for all i.
+
+Find the shortest path from src(0) vertex to all the vertices and if it is impossible to reach any vertex, then return -1 for that vertex.
+
+Examples :
+
+Input: V = 4, E = 2, edges = [[0,1,2], [0,2,1]]
+Output: [0, 2, 1, -1]
+Explanation: Shortest path from 0 to 1 is 0->1 with edge weight 2. Shortest path from 0 to 2 is 0->2 with edge weight 1. There is no way we can reach 3, so it's -1 for 3.
+Input: V = 6, E = 7, edges = [[0,1,2], [0,4,1], [4,5,4], [4,2,2], [1,2,3], [2,3,6], [5,3,1]]
+Output: [0, 2, 3, 6, 1, 5]
+Explanation: Shortest path from 0 to 1 is 0->1 with edge weight 2. Shortest path from 0 to 2 is 0->4->2 with edge weight 1+2=3. Shortest path from 0 to 3 is 0->4->5->3 with edge weight 1+4+1=6. Shortest path from 0 to 4 is 0->4 with edge weight 1.Shortest path from 0 to 5 is 0->4->5 with edge weight 1+4=5.
+"""
+
 def findAdj(V, edges):
     adj = [[] for _ in range(V)]
     for i in range(V):
